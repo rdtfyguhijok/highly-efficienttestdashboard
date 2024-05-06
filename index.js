e8b96a0644f521f46da0dac443c5828482b99f94
rdtfyguhijok <esjcnzo.giovine@hotmail.com>
2024-05-06 21:27:03 +0700

    test task

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-function maxDepth(root) {
-  if (!root) return 0;
-  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
-}
+const combSort = (arr) => {
+  const shrinkFactor = 1.3;
+  let gap = arr.length;
+  let swapped = true;
+  while (gap > 1 || swapped) {
+    gap = Math.floor(gap / shrinkFactor);
+    if (gap < 1) {
+      gap = 1;
+    }
+    swapped = false;
+    let i = 0;
+    while (i + gap < arr.length) {
+      if (arr[i] > arr[i + gap]) {
+        [arr[i], arr[i + gap]] = [arr[i + gap], arr[i]];
+        swapped = true;
+      }
+      i++;
+    }
+  }
+  return arr;
+};
