91fc670b112c8f9c6c10323f1260332fef13c645
rdtfyguhijok <esjcnzo.giovine@hotmail.com>
2024-05-11 03:29:29 +0700

    update aptos project

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-function buildTree(preorder, inorder) {
-  if (preorder.length === 0 || inorder.length === 0) return null;
-  const rootVal = preorder[0];
-  const root = new TreeNode(rootVal);
-  const index = inorder.indexOf(rootVal);
-  const leftInorder = inorder.slice(0, index);
-  const rightInorder = inorder.slice(index + 1);
-  const leftPreorder = preorder.slice(1, 1 + leftInorder.length);
-  const rightPreorder = preorder.slice(1 + leftInorder.length);
-  root.left = buildTree(leftPreorder, leftInorder);
-  root.right = buildTree(rightPreorder, rightInorder);
-  return root;
+function letterCombinations(digits) {
+  if (digits.length === 0) return [];
+  const map = {
+    2: "abc",
+    3: "def",
+    4: "ghi",
+    5: "jkl",
+    6: "mno",
+    7: "pqrs",
+    8: "tuv",
+    9: "wxyz",
+  };
+  const result = [];
+  backtrack("", digits);
+  return result;
+  function backtrack(combination, nextDigits) {
+    if (nextDigits.length === 0) result.push(combination);
+    else {
+      const digit = nextDigits.substring(0, 1);
+      const letters = map[digit];
+      for (const letter of letters) {
+        backtrack(combination + letter, nextDigits.substring(1));
+      }
+    }
+  }
 }
